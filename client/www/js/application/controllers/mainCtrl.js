d96906e07bb91310202d2f7da5f8d9c07831839b
Rashid Mammadov <reshid@live.com>
2018-01-02 19:14:29 +0300

    Added bower.json for package management.

--- client/www/js/application/controllers/mainCtrl.js
+++ client/www/js/application/controllers/mainCtrl.js
@@ -2,7 +2,14 @@
     'use strict';
 
     function MainCtrl($scope) {
+        var self = this;
+        this.test;
 
+        function change(){
+            self.test = "ozelden.com";
+        }
+
+        this.change = change;
     }
 
     angular.module('ozelden.controllers').controller('MainCtrl', MainCtrl);
