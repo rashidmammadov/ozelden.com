cb7eeb9673b336b7a19a16df4d1b1bc67853c093
Rashid Mammadov <reshid@live.com>
2018-01-21 15:54:09 +0300

    Updated front-end of tutor registration.
    Signed-off-by: Rashid Mammadov <reshid@live.com>

--- client/platforms/android/assets/www/js/application/controllers/mainCtrl.js
+++ client/platforms/android/assets/www/js/application/controllers/mainCtrl.js
@@ -1,15 +1,30 @@
 (function () {
     'use strict';
 
-    function MainCtrl($scope) {
+    /**
+     * @ngdoc controller
+     * @name ozelden.controllers.controllers:MainCtrl
+     * @description Controller for the main page view.
+     */
+    function MainCtrl($scope, $translate) {
         var self = this;
-        this.test;
 
-        function change(){
-            self.test = "ozelden.com";
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:MainCtrl#changeLangugage
+         * @description This method changes application`s language.
+         * @param lang: send selected language name.
+         */
+        function changeLanguage(lang){
+            $translate.use(lang);
         }
 
-        this.change = change;
+        function register() {
+
+        }
+
+        this.changeLanguage = changeLanguage;
+        this.register = register;
     }
 
     angular.module('ozelden.controllers').controller('MainCtrl', MainCtrl);
--- client/platforms/android/assets/www/js/application/modules/ozelden.js
+++ client/platforms/android/assets/www/js/application/modules/ozelden.js
@@ -2,10 +2,19 @@
     'use strict';
 
     var ozelden = angular.module('ozelden',['ozelden.controllers','ozelden.directives','ozelden.filters','ozelden.services',
-        'ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages', 'ui.router']);
+        'ngAnimate', 'ngAria', 'ngMaterial', 'ngMessages', 'pascalprecht.translate', 'ui.router']);
 
-    ozelden.config(function ($stateProvider, $urlRouterProvider) {
+    ozelden.config(function ($stateProvider, $urlRouterProvider, $translateProvider, $mdIconProvider) {
 
+        // load application language.
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'data/lang/',
+            suffix: '.json'
+        });
+        $translateProvider.registerAvailableLanguageKeys(['az', 'en', 'tr']);
+        $translateProvider.preferredLanguage('tr');
+
+        // define states for router.
         $urlRouterProvider.otherwise("/");
 
         $stateProvider.state('ozelden',{
@@ -13,6 +22,16 @@
             templateUrl: 'html/controllers/main.html',
             controller: 'MainCtrl',
             controllerAs: 'Main'
-        })
+        }).state('ozelden.tutorLogin',{
+            url: 'tutor/login',
+            templateUrl: 'html/controllers/tutorLogin.html',
+            controller: 'TutorLoginCtrl',
+            controllerAs: 'tutor'
+        });
+
+        // define icons.
+        $mdIconProvider.icon('briefcase', 'img/icon/briefcase.svg');
+        $mdIconProvider.icon('female', 'img/icon/female.svg');
+        $mdIconProvider.icon('male', 'img/icon/male.svg');
     })
 }());
