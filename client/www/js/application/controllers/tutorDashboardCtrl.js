25b403f5700261d9d8ba5867ee56bb1519ec8c03
Rashid Mammadov <reshid@live.com>
2018-06-10 00:09:23 +0300

    Updated search directive.
    Signed-off-by: Rashid Mammadov <reshid@live.com>

--- client/www/js/application/controllers/tutorDashboardCtrl.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    function TutorDashboardCtrl($scope, $rootScope) {
-
-    }
-
-    angular.module('ozelden.controllers').controller('TutorDashboardCtrl', TutorDashboardCtrl);
-})();
