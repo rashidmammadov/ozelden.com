571a4628b2bd8872b5f8778b6e64ff4a7ec39cf4
Rəşid Məmmədov <reshid@live.com>
2018-12-06 12:14:50 +0000

    Updated userLecturesListCtrl.js

--- client/www/js/application/controllers/userLecturesListCtrl.js
+++ client/www/js/application/controllers/userLecturesListCtrl.js
@@ -11,7 +11,7 @@
         this.lecturesList = [];
 
         this.experienceList = [];
-        for (var i=0; i<=60; i++) { self.experienceList.push(i); }
+        for (var i = 0; i <= 60; i++) { self.experienceList.push(i); }
 
         /**
          * @ngdoc request
@@ -41,24 +41,16 @@
          * @name ozelden.controllers.controllers:UserLecturesListCtrl#addLecture
          * @description Add created lecture to lectures list if not exist.
          */
-        function addLecture(){
+        function addLecture() {
             $rootScope.loadingOperation = true;
-            var lectureObject = {
-                lectureArea: self.selectedLectureArea.base,
-                lectureTheme: self.selectedLectureTheme.base,
-                experience: self.selectedExperience,
-                price: self.selectedPrice,
-                currency: "TRY",
-                average: self.selectedLectureTheme.average.TRY
-            };
+            var lectureObject = $$setLectureObject();
 
-            var result = self.lecturesList.find(function(lecture){
-                return lecture.lectureArea === lectureObject.lectureArea &&
-                        lecture.lectureTheme === lectureObject.lectureTheme;
+            var result = self.lecturesList.find(function(lecture) {
+                return $$isEqualLectures(lecture, lectureObject);
             });
 
-            if (!result){
-                UserSettingService.addToUserLectureList(lectureObject).then(function(result){
+            if (!result) {
+                UserSettingService.addToUserLectureList(lectureObject).then(function(result) {
                     self.lecturesList.push(lectureObject);
                     $rootScope.loadingOperation = false;
                     NotificationService.showMessage($filter('translate')(result.message));
@@ -88,20 +80,57 @@
                 .cancel($filter('translate')('CANCEL'));
 
             $mdDialog.show(confirm).then(function() {
-                TutorService.removeTutorLecture(lecture).then(function(result){
-                    self.lecturesList = self.lecturesList.filter(function (value) {
-                        return !(value.lectureArea === lecture.lectureArea &&
-                            value.lectureTheme === lecture.lectureTheme);
-                    });
-                    $rootScope.loadingOperation = false;
-                    NotificationService.showMessage($filter('translate')(result));
-                },function (failure) {
-                    $rootScope.loadingOperation = false;
-                    NotificationService.showMessage($filter('translate')(failure));
-                });
+                $$confirmToRemoveLecture(lecture);
             }, function() {
                 $rootScope.loadingOperation = false;
             });
+        }
+
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:UserLecturesListCtrl#$$confirmToRemoveLecture
+         * @description Remove lecture from user`s lectures list.
+         * @param {Object} lecture - Holds the selected lecture data.
+         */
+        function $$confirmToRemoveLecture(lecture) {
+            TutorService.removeLectureFromUserLectureList(lecture).then(function(result) {
+                self.lecturesList = self.lecturesList.filter(function (value) {
+                    return !$$isEqualLectures(value, lecture);
+                });
+                $rootScope.loadingOperation = false;
+                NotificationService.showMessage($filter('translate')(result));
+            },function (failure) {
+                $rootScope.loadingOperation = false;
+                NotificationService.showMessage($filter('translate')(failure));
+            });
+        }
+
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:UserLecturesListCtrl#$$isEqualLectures
+         * @description Check if given lectures is euqal.
+         * @param {Object} a - Holds the first given lecture object.
+         * @param {Object} b - Holds the second given lecture object.
+         */
+        function $$isEqualLectures(a, b) {
+            return a.lectureArea === b.lectureArea && a.lectureTheme === b.lectureTheme;
+        }
+
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:UserLecturesListCtrl#$$setLectureObject
+         * @description Set lecture object with selected values.
+         */
+        function $$setLectureObject() {
+            var lectureObject = {
+                lectureArea: self.selectedLectureArea.base,
+                lectureTheme: self.selectedLectureTheme.base,
+                experience: self.selectedExperience,
+                price: self.selectedPrice,
+                currency: "TRY",
+                average: self.selectedLectureTheme.average.TRY
+            };
+            return lectureObject;
         }
 
         this.addLecture = addLecture;
