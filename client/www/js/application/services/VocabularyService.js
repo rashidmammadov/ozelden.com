a3d992896c0769e09532be6eeb48b2f5c9cb4a6e
Rəşid Məmmədov <reshid@live.com>
2018-12-06 12:31:42 +0000

    Updated VocabularyService.js

--- client/www/js/application/services/VocabularyService.js
+++ client/www/js/application/services/VocabularyService.js
@@ -7,10 +7,10 @@
         this.api = DirectionService.apiUri();
 
         this.data = function() { return self.api + 'data'; }
-        this.lecturesList = function() { return self.api + 'lecturesList'; }
         this.suitabilitySchedule = function() { return self.api + 'suitabilitySchedule'; }
         this.refreshUser = function() { return  self.api + 'refreshUser'; }
         this.search = function() { return self.api + 'search/get'; }
+        this.userLecturesList = function() { return self.api + 'userLecturesList'; }
         this.userLogin = function() { return self.api + 'login'; }
         this.userLogout = function() { return self.api + 'logout'; }
         this.userRegister = function() { return self.api + 'register'; }
