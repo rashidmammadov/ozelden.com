25be0f0287cfdbe75958ca47fc06c905a8b92d9d
Rashid Mammadov <reshid@live.com>
2019-01-17 23:15:28 +0300

    Updated lecturesList.
    Signed-off-by: Rashid Mammadov <reshid@live.com>

---
+++ client/www/js/application/controllers/dialog/classDialogCtrl.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    function ClassDialogCtrl() {
+        var self = this;
+
+    }
+
+    angular.module('ozelden.controllers').controller('ClassDialogCtrl', ClassDialogCtrl);
+}());
