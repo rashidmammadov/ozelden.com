55f464f628e4085db23e2bf8402034b7e7a7ffb6
Rashid Mammadov <reshid@live.com>
2019-02-11 18:17:01 +0300

    Added child controller.

---
+++ api/app/Child.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Child extends Model {
+
+    protected $table = 'children';
+    protected $primaryKey = CHILD_ID;
+
+    protected $fillable = [
+        CHILD_ID, TYPE, USER_ID, PICTURE, NAME, SURNAME, SEX, BIRTH_DATE
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+}
