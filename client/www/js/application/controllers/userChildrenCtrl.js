7e52c26cb959285983f8782e6b2de82b7176c38a
Rashid Mammadov <reshid@live.com>
2019-02-12 16:23:46 +0300

    Updated child controller.

--- client/www/js/application/controllers/userChildrenCtrl.js
+++ client/www/js/application/controllers/userChildrenCtrl.js
@@ -1,26 +1,31 @@
 (function () {
     'use strict';
 
-    function UserChildrenCtrl($mdDialog) {
+    function UserChildrenCtrl($rootScope, $mdDialog, $filter, ChildService, NotificationService) {
         var self = this;
+        $rootScope.loadingOperation = true;
 
-        this.childList = [{
-            childId: 1,
-            name: 'Rashid',
-            surname: 'Mammadov',
-            classes: [{
-                className: 'my first class'
-            },{
-                className: 'my second class'
-            },{
-                className: 'my third class'
-            }]
-        },{
-            childId: 1,
-            name: 'Rashid',
-            surname: 'Mammadov'
-        }];
+        this.childList = [];
 
+        function getChildList() {
+            ChildService.getUserChildren().then(function (result) {
+                $rootScope.loadingOperation = false;
+                self.childList = result.data;
+            }, function () {
+                $rootScope.loadingOperation = false;
+            });
+        }
+
+        getChildList();
+
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:UserChildrenCtrl#openChildrenDialog
+         * @description Open dialog with given operation and data.
+
+         * @param {String} operation - holds the operation name.
+         * @param {Object=} data - holds the child data.
+         */
         function openChildrenDialog(operation, data) {
             if (operation !== 'remove') {
                 $mdDialog.show({
@@ -32,8 +37,78 @@
                         data: data
                     },
                     clickOutsideToClose: true
+                }).then(function (child) {
+                    if (operation === 'create') {
+                        $$createChild(child);
+                    } else if (operation === 'update') {
+                        $$updateChild(child);
+                    }
                 })
+            } else {
+                var confirm = $mdDialog.confirm()
+                    .title($filter('translate')('REMOVE_CHILD_TITLE'))
+                    .textContent($filter('translate')('REMOVE_CHILD_CONTENT'))
+                    .ariaLabel('Remove Child')
+                    .ok($filter('translate')('CONFIRM'))
+                    .cancel($filter('translate')('CANCEL'));
+
+                $mdDialog.show(confirm).then(function() {
+                    $rootScope.loadingOperation = true;
+                    $$removeChild(data.childId);
+                }, function () {
+                    $rootScope.loadingOperation = false;
+                });
             }
+        }
+
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:UserChildrenCtrl#$$createChild
+         * @description Add new child to user`s child list.
+
+         * @param {Object} child - holds the child data.
+         */
+        function $$createChild(child) {
+            $rootScope.loadingOperation = true;
+            ChildService.addNewChild(child).then(function (result) {
+                $rootScope.loadingOperation = false;
+                NotificationService.showMessage($filter('translate')(result.message));
+                getChildList();
+            }, function () {
+                $rootScope.loadingOperation = false;
+                NotificationService.showMessage($filter('translate')('SOMETHING_WENT_WRONG'));
+            });
+        }
+
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:UserChildrenCtrl#$$removeChild
+         * @description Remove the given child.
+
+         * @param {Integer} childId - holds the child data.
+         */
+        function $$removeChild(childId) {
+            ChildService.removeChild({childId: childId}).then(function (result) {
+                $rootScope.loadingOperation = false;
+                if (result.status === 'success') {
+                    self.childList = self.childList.filter(function (d) { return d.childId !== childId; });
+                    NotificationService.showMessage($filter('translate')(result.message));
+                }
+            }, function () {
+                $rootScope.loadingOperation = false;
+                NotificationService.showMessage($filter('translate')('SOMETHING_WENT_WRONG_WHILE_REMOVING_CHILD'));
+            })
+        }
+
+        /**
+         * @ngdoc method
+         * @name ozelden.controllers.controllers:UserChildrenCtrl#$$updateChild
+         * @description Update the given child data.
+
+         * @param {Object} child - holds the child data.
+         */
+        function $$updateChild(child) {
+            
         }
 
         this.openChildrenDialog = openChildrenDialog;
