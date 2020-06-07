d5fef6e4a39e414150e98ca287fe23e0f15177b9
Rashid Mammadov <reshid@live.com>
2020-06-07 16:34:40 +0300

    updated paid services price.

--- web/src/app/constants/paid.constant.ts
+++ web/src/app/constants/paid.constant.ts
@@ -1,29 +1,29 @@
 export const PAID = {
     BID: [
-        { key: 'BID1', price: 1.5, value: 1 },
-        { key: 'BID3', price: 3, value: 3 },
-        { key: 'BID5', price: 4.5, value: 5 },
-        { key: 'BID10', price: 8, value: 10 },
-        { key: 'BID30', price: 21, value: 30 },
-        { key: 'BID50', price: 30, value: 50 },
-        { key: 'BID100', price: 50, value: 100 }
+        { key: 'BID1', price: 0.75, value: 1 },
+        { key: 'BID3', price: 1.5, value: 3 },
+        { key: 'BID5', price: 2.5, value: 5 },
+        { key: 'BID10', price: 4, value: 10 },
+        { key: 'BID30', price: 10, value: 30 },
+        { key: 'BID50', price: 15, value: 50 },
+        { key: 'BID100', price: 25, value: 100 }
     ],
     BOOST: [
-        { key: 'BOOST1', price: 3, value: 1 },
-        { key: 'BOOST3', price: 6, value: 3 },
-        { key: 'BOOST7', price: 9, value: 7 },
-        { key: 'BOOST15', price: 12, value: 15 },
-        { key: 'BOOST30', price: 15, value: 30 },
-        { key: 'BOOST180', price: 60, value: 180 },
-        { key: 'BOOST365', price: 90, value: 365 }
+        { key: 'BOOST1', price: 1.5, value: 1 },
+        { key: 'BOOST3', price: 3, value: 3 },
+        { key: 'BOOST7', price: 4.5, value: 7 },
+        { key: 'BOOST15', price: 6, value: 15 },
+        { key: 'BOOST30', price: 8, value: 30 },
+        { key: 'BOOST180', price: 30, value: 180 },
+        { key: 'BOOST365', price: 45, value: 365 }
     ],
     RECOMMEND: [
-        { key: 'RECOMMEND1', price: 6, value: 1 },
-        { key: 'RECOMMEND3', price: 12, value: 3 },
-        { key: 'RECOMMEND7', price: 18, value: 7 },
-        { key: 'RECOMMEND15', price: 24, value: 15 },
-        { key: 'RECOMMEND30', price: 30, value: 30 },
-        { key: 'RECOMMEND180', price: 120, value: 180 },
-        { key: 'RECOMMEND365', price: 180, value: 365 },
+        { key: 'RECOMMEND1', price: 3, value: 1 },
+        { key: 'RECOMMEND3', price: 6, value: 3 },
+        { key: 'RECOMMEND7', price: 9, value: 7 },
+        { key: 'RECOMMEND15', price: 12, value: 15 },
+        { key: 'RECOMMEND30', price: 16, value: 30 },
+        { key: 'RECOMMEND180', price: 60, value: 180 },
+        { key: 'RECOMMEND365', price: 90, value: 365 },
     ]
 };
